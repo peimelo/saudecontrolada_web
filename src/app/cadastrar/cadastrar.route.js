e7786af6fca05eb569ae7bb9133e18fec80813da
Melo <peimelo@gmail.com>
2016-02-16 10:41:24 -0200

    Adicionada tela de cadastrar

--- src/app/cadastrar/cadastrar.route.js
+++ src/app/cadastrar/cadastrar.route.js
@@ -1,19 +1,19 @@
-//(function() {
-//  'use strict';
-//
-//  angular
-//    .module('app')
-//    .config(routeConfig);
-//
-//  /** @ngInject */
-//  function routeConfig($stateProvider) {
-//    $stateProvider
-//      .state('cadastrar', {
-//        url: '/cadastrar',
-//        templateUrl: 'app/cadastrar/cadastrar.html',
-//        controller: 'CadastrarController',
-//        controllerAs: 'vm'
-//      });
-//  }
-//
-//})();
+(function() {
+  'use strict';
+
+  angular
+    .module('app')
+    .config(routeConfig);
+
+  /** @ngInject */
+  function routeConfig($stateProvider) {
+    $stateProvider
+      .state('cadastrar', {
+        url: '/cadastrar',
+        templateUrl: 'app/cadastrar/cadastrar.html',
+        controller: 'CadastrarController',
+        controllerAs: 'vm'
+      });
+  }
+
+})();
