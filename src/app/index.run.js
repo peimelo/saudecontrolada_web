c9cacff393ddd21174c275decf424dfb4653495f
Paulo Melo <peimelo@gmail.com>
2016-06-12 17:21:39 -0300

    Icon hear in blockui'

--- src/app/index.run.js
+++ src/app/index.run.js
@@ -27,6 +27,7 @@
         '<div class="block-ui-overlay"></div> \
         <div class="block-ui-message-container" aria-live="assertive" aria-atomic="true"> \
           <div class="block-ui-message" ng-class="$_blockUiMessageClass"> \
+            <p class="text-center"><i class="fa fa-heartbeat fa-2x"></i></p>  \
             <div class="sk-spinner sk-spinner-three-bounce"> \
               <div class="sk-bounce1"></div> \
               <div class="sk-bounce2"></div> \
