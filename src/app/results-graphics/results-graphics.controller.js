f6db88cd4cb9fd01e97fdb9a46eecd3a26b671a6
Paulo Melo <peimelo@gmail.com>
2016-11-02 16:26:07 -0200

    Exibindo os gráficos

--- src/app/results-graphics/results-graphics.controller.js
+++ src/app/results-graphics/results-graphics.controller.js
@@ -9,6 +9,36 @@
   function ResultsGraphicsController(ResultsGraphicsResource, $stateParams, toaster) {
     var vm = this;
 
+    vm.exam = {};
+    vm.getGraphic = getGraphic;
+    vm.flotData = [{ label: 'Valor', data: [] }];
+    vm.flotOptions = {
+      xaxis: {
+        // dayNames: ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sáb"],
+        minTickSize: [1, "hour"],
+        mode: "time",
+        monthNames: [
+          'Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'
+        ]
+      },
+      colors: ["#1ab394"],
+      grid: {
+        color: "#999999",
+        hoverable: true,
+        clickable: true,
+        tickColor: "#D4D4D4",
+        borderWidth: 0
+      },
+      tooltip: true,
+      tooltipOpts: {
+        content: function(label, xval, yval) {
+          var content = "%s em " + moment(xval).utcOffset(0).format('DD/MM/YYYY HH:mm') + ' = ' + yval;
+          return content;
+        },
+        xDateFormat: "%y-%0m-%0d",
+        onHover: function (flotItem, $tooltipEl) {}
+      }
+    };
     vm.pagination = { currentPage: ($stateParams.page || 1) };
     vm.resultsGraphics = [];
     vm.query = query;
@@ -17,6 +47,30 @@
 
     function activate() {
       query();
+    }
+
+    function getChart(graphicValues) {
+      var qtde = graphicValues.length;
+      var flotChart = [];
+
+      for (var i = qtde - 1; i >= 0; i--) {
+        flotChart.push([
+          moment(graphicValues[i].date).utcOffset(0).valueOf(),
+          graphicValues[i].value
+        ]);
+      }
+
+      vm.flotData[0].data = flotChart;
+    }
+
+    function getGraphic(result) {
+      vm.exam = result;
+
+      ResultsGraphicsResource.get({ id: result.exam_id },
+        function(response) {
+          getChart(response.exam_results);
+        }
+      );
     }
 
     function query() {
