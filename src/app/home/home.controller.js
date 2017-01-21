4f70a8deecbd7254a463a29418cb9abeb3f17ec4
Paulo Melo <peimelo@gmail.com>
2017-01-20 22:55:48 -0200

    Atualizada na página inicial

--- src/app/home/home.controller.js
+++ src/app/home/home.controller.js
@@ -11,20 +11,12 @@
 
     vm.presentations = [
       {
-        title: 'Responsivo',
-        text: 'Significa que você pode acessar do seu computador, tablet ou celular.'
-      },
-      {
-        title: 'Controle de Peso',
-        text: 'Controle a evolução do seu peso, o peso ideal e o IMC.'
-      },
-      {
-        title: 'Resultados de Exames',
-        text: 'Todos os seus resultados em um só lugar e com gráficos.'
-      },
-      {
         title: 'Gratuito',
         text: 'Você não precisa pagar nada. Queremos ajudar você a cuidar da sua saúde.'
+      },
+      {
+        title: 'Responsivo',
+        text: 'Significa que você pode acessar do seu computador, tablet ou celular.'
       }
     ];
 
